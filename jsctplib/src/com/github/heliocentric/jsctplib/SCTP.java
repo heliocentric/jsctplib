71a2759962e357cee8b82b0f26b0e26942eb7190
helio <helio@helio-Satellite-L305>
2013-02-09 17:59:59 -0500

    * Add the Datagram receive loop.

--- jsctplib/src/com/github/heliocentric/jsctplib/SCTP.java
+++ jsctplib/src/com/github/heliocentric/jsctplib/SCTP.java
@@ -4,7 +4,12 @@
  */
 package com.github.heliocentric.jsctplib;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 /**
@@ -12,22 +17,79 @@ import java.net.DatagramSocket;
  * @author helio
  */
 public class SCTP {
+	
 	public SCTP() {
+		this.Port = 9899;
 	}
-	private Thread IOAgent;
-	private Thread MultiPlexAgentIn;
-	private Thread MultiPlexAgentOut;
+	
+	public SCTP(int Port) {
+		this.Port = Port;
+	}
+	
+	private int Port;
 	private DatagramSocket Socket;
+	
+	private Thread IOInputAgentThread;
+	private Thread IOOutputAgentThread;
+	private Thread MultiplexInputAgentThread;
+	private Thread MultiplexOutputAgentThread;
+	
 	public SCTPSocket socket(int domain, int type, int protocol) {
 		return new SCTPSocket(this);
 	}
-	public void Start() {
-		
-	}
-	public void Stop() {
-		
-	}
+	
 	public void close(SCTPSocket socket) {
+		
+	}
+	
+	public void Start() {
+		try {
+			this.Socket = new DatagramSocket(this.Port);
+		} catch (SocketException ex) {
+			Logger.getLogger(SCTP.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		
+		this.IOInputAgentThread = new Thread(new IOInputAgent(this));
+		this.IOInputAgentThread.start();
+	}
+	
+	private class IOInputAgent implements Runnable {
+
+		private SCTP Parent;
+		public IOInputAgent(SCTP parent) {
+			this.Parent = parent;
+		}
+		@Override
+		public void run() {
+			byte[] buffer;
+			
+			boolean end = false;
+			while(end == false) {
+				try {
+					buffer = new byte[4096];
+					DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+					Parent.Socket.receive(packet);
+					System.out.println("Packet Received");
+				} catch (IOException ex) {
+					Logger.getLogger(SCTP.class.getName()).log(Level.SEVERE, null, ex);
+				}
+				
+			}
+		}
+	}
+	private class IOOutputAgent implements Runnable {
+		private SCTP Parent;
+		public IOOutputAgent(SCTP parent) {
+			this.Parent = parent;
+		}
+
+		@Override
+		public void run() {
+		}
+		
+	}
+	
+	public void Stop() {
 		
 	}
 }
