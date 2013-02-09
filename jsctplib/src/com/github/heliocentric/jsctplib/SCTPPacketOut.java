7eeee436b932320b63fefbd5d3abc401fcd7bdfb
helio <helio@helio-Satellite-L305>
2013-02-09 16:30:52 -0500

    * Move SCTPPacketOut

---
+++ jsctplib/src/com/github/heliocentric/jsctplib/SCTPPacketOut.java
@@ -0,0 +1,27 @@
+package com.github.heliocentric.jsctplib;
+
+
+import com.github.heliocentric.jsctplib.SCTPPacket;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author helio
+ */
+public class SCTPPacketOut implements SCTPPacket {
+
+	@Override
+	public void Unpack(byte[] packet) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public byte[] Pack() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+	
+}
