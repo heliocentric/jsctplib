1b892878016b6f882a1c187264c8d432aa1187b8
helio <helio@helio-Satellite-L305>
2013-02-09 16:18:17 -0500

    * Add an echo based test shim.

---
+++ echoserver/src/echoserver/Echoserver.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package echoserver;
+
+import com.github.heliocentric.jsctplib.SCTP;
+
+/**
+ *
+ * @author helio
+ */
+public class Echoserver {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		SCTP engine;
+		engine = new SCTP();
+		
+	}
+}
